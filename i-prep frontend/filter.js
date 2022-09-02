27fb8a984ef3ead871c612562205fc3e1b941c7e
rsiree <92039117+rsiree@users.noreply.github.com>
2022-09-03 01:11:02 +0530

    updated filter

--- i-prep frontend/filter.js
+++ i-prep frontend/filter.js
@@ -1,6 +1,6 @@
 
 
-function fetchData(cat, random) {
+function fetchData(cat) {
     fetch(`https://vast-stream-94685.herokuapp.com/quiz`)
         .then(res => res.json())
         .then(data => {
@@ -8,13 +8,17 @@ function fetchData(cat, random) {
             let show;
             if (cat !== "Random") {
                 let data2 = data.filter((e) => e.category === cat);
-                show = data2.find((e) => e.id === random)
-                console.log(show,"o")
+                // show = data2.find((e) => e.id === random);
+                let r = Math.floor(Math.random() * data2.length);
+                // console.log(r,data2.length,data2[0])
+                show = data2[0];
             } else {
-                console.log(cat,random)
+                let random = Math.floor(Math.random() * data.length + 1);
+                console.log(random)
                 show = data.find((e) => e.id === random);
                 console.log(show,"r")
             }
+
             document.getElementById("ans").innerHTML ="A"+" : "+ show.answer;
             document.getElementById("qn").innerHTML ="Q"+" : "+ show.question;
             document.getElementById("cat").innerHTML ="Category"+" :"+" "+ show.category;
@@ -25,39 +29,32 @@ function fetchData(cat, random) {
 
 var reactBtn=document.getElementById("react");
 reactBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 5+10);
-    fetchData(reactBtn.textContent,random)
+    fetchData(reactBtn.textContent)
 }); 
 
 var reduxBtn=document.getElementById("redux");
 reduxBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 5+16);
-
-    fetchData(reduxBtn.textContent,random)
+    fetchData(reduxBtn.textContent)
 });
 
 var jsBtn=document.getElementById("js");
 jsBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 5+21);
-    fetchData(jsBtn.textContent,random)
+    fetchData(jsBtn.textContent)
 });
 
 var htmlBtn=document.getElementById("html");
 htmlBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 5+1);
-    fetchData(htmlBtn.textContent,random)
+    fetchData(htmlBtn.textContent)
 });
 
 var cssBtn=document.getElementById("css");
 cssBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 5+6);
-    fetchData(cssBtn.textContent,random)
+    fetchData(cssBtn.textContent)
 });
 
 var randomBtn=document.getElementById("random");
 randomBtn.addEventListener("click", function () {
-    let random = Math.floor(Math.random() * 25+1);
-    fetchData(randomBtn.textContent,random)
+    fetchData(randomBtn.textContent)
 });
 
 
